6073a00b2ecd8f52b262daaf41a0aef264593c3a
Xu Tian Guang <563492886@qq.com>
2018-03-12 14:30:21 +0800

    zxyidong

---
+++ zxmin/js/wap.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+    var deviceWidth=$(window).outerWidth();
+    if(deviceWidth>750){
+        $("html").css("font-size","100px");
+    }else{
+        $("html").css("font-size",deviceWidth/750*100+'px');
+    }
+
+     $(function(){
+         $('#gotop').click(function(){
+            $('html , body').animate({scrollTop: 0},'500');
+         });
+     });
+     
+});
